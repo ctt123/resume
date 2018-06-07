40256ecee5c98ab405edead9e08c6ed9b457839d
ctt123 <283569035@qq.com>
2018-06-07 22:51:48 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -22,8 +22,21 @@ module.exports = {
   // add your custom rules here
   rules: {
     // allow async-await
-    'generator-star-spacing': 'off',
+    // 'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // "indent": ["error", 0, {"SwitchCase": 1}],
+    'semi': 0,
+    'no-new': 0,
+    'no-proto': 0,
+    'eol-last': 0,
+    'camelcase': 0,
+    'space-before-function-paren': 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
