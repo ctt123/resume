b47c2859fab37bdebd2e9b4d5a63a60d55464ca1
ctt123 <283569035@qq.com>
2018-07-06 19:10:41 +0800

    登录

--- src/components/page/login/SignIn.vue
+++ src/components/page/login/SignIn.vue
@@ -2,12 +2,11 @@
   <page class="login">
     <div class="content">
       <h3 class="title">登录</h3>
-      <el-input v-model="user.account" :placeholder="Labeler('p_login_name')">
+      <el-input v-model="user.account" :placeholder="Labeler('p_login_name')" class="form-row">
         <i slot="prefix" class="el-input__icon el-icon-search"></i></el-input>
-      <el-input v-model="user.password" placeholder="请输入密码">
+      <el-input v-model="user.password" placeholder="请输入密码" class="form-row">
         <i slot="prefix" class="el-input__icon el-icon-setting"></i></el-input>
-      <el-button type="success" @click="login"><Label code="l_login"/></el-button>
-      <span @click="test">goHome</span>
+      <el-button @click="login" class="form-row form-btn"><Label code="l_login"/></el-button>
     </div>
   </page>
 </template>
@@ -30,9 +29,6 @@ export default {
       BaseApi.login(this.user).then(data => {
 
       })
-    },
-    test () {
-      this.$router.replace({name: 'chat'})
     }
   },
   mounted () {
@@ -51,7 +47,7 @@ export default {
     top: 50%
     left: 50%
     width: 300px
-    border: 1px solid #eee
+    /*border: 1px solid #eee*/
     padding: 40px
     text-align: center
     background-color: #fff
@@ -61,4 +57,14 @@ export default {
     -o-transform: translate(-50%, -50%)
     transform: translate(-50%, -50%)
 
+    border-radius: 8px
+    box-shadow: 1px 2px 15px rgba(0, 0, 0, .3)
+
+  .form-row
+    margin-top: 26px
+
+  .form-btn
+    background-color: #5cd6c9
+    color: #ffffff
+    border: none
 </style>
